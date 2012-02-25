143c3664deb6b0dc6851f2eb0543f8036a6e50b1
perra044 <perra044@e5a9bd0e-96e4-4e5d-b9da-31dc9101860d>
2012-02-25 19:20:56 +0000

    Added bootstrap data for Groups and StudentGroupAssignment
    git-svn-id: http://cscirepo.morris.umn.edu/svn/csci-3601/Spring12/ReleaseOne/TeamAmish@8598 e5a9bd0e-96e4-4e5d-b9da-31dc9101860d

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -3,7 +3,8 @@ import surveyTool.Student;
 import surveyTool.Enrollment;
 import surveyTool.Manager;
 import surveyTool.Project;
-
+import surveyTool.Group;
+import surveyTool.StudentGroupAssignment;
 
 class BootStrap {
 
@@ -23,6 +24,13 @@ class BootStrap {
 		
 		Project butterChurning = new Project(name:'Butter Churning', course:amish101).save(failOnError:true)
 		Project gatherHay = new Project(name:'Gathering Hay', course:basketWeaving).save(failOnError:true)
+		
+		Group awesomeHats = new Group(name:'Awesome Hats', project:butterChurning).save(failOnError:true)
+		Group beardedBandits = new Group(name:'Bearded Bandits', project:gatherHay).save(failOnError:true)
+		Group animalLovers = new Group(name:'Animal Lovers', project:gatherHay).save(failOnError:true)
+		
+		StudentGroupAssignment assignment = new StudentGroupAssignment(student:roger, group:awesomeHats)
+		StudentGroupAssignment assignment2 = new StudentGroupAssignment(student:gertrude, group:awesomeHats)
 		
 		
 		
